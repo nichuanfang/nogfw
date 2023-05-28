e3a021b92a9de66c133549759a8920e314feaa51
倪传方 <1290274972@qq.com>
2023-05-28 21:00:14 +0800

    🧪 test: trigger github action

--- youtube.py
+++ youtube.py
@@ -1,11 +1,19 @@
 #!/usr/local/bin/python
 # coding=utf-8
 from time import sleep
+import cv2
 import requests
 import logging
 import io
 import sys
 import subprocess
+
+def qr_recognize(file_path:str):
+    qrcode_filename = file_path
+    qrcode_image = cv2.imread(qrcode_filename)
+    qrCodeDetector = cv2.QRCodeDetector()
+    data, bbox, straight_qrcode = qrCodeDetector.detectAndDecode(qrcode_image)
+    return data
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 
@@ -17,4 +25,5 @@ logging.basicConfig(level=logging.INFO)
 for index in range(2):
     subprocess.call(f'ffmpeg -i "$(yt-dlp -g qmRkvKo-KbQ | head -n 1)" -vframes 1 dist/last_{index}.jpg',shell=True)
     # 处理生成的二维码 生成节点信息
+    qr_recognize(f'dist/last_{index}.jpg')
     sleep(30)
