43224165b286932223a7686ecc5fb5f6d073a349
倪传方 <1290274972@qq.com>
2023-05-28 20:53:04 +0800

    🧪 test: trigger github action

--- youtube.py
+++ youtube.py
@@ -14,7 +14,7 @@ logging.basicConfig(level=logging.INFO)
 # 隔一段时间获取二维码
 # ffmpeg -i "$(yt-dlp -g qmRkvKo-KbQ | head -n 1)" -vframes 1 dist/last.jpg
 
-for index in range(60):
+for index in range(2):
     subprocess.call(f'ffmpeg -i "$(yt-dlp -g qmRkvKo-KbQ | head -n 1)" -vframes 1 dist/last_{index}.jpg',shell=True)
     # 处理生成的二维码 生成节点信息
     sleep(30)
