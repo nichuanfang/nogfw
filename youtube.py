d519dc219a3ebd3e5ffb64cf4fcb1f676be959ef
倪传方 <1290274972@qq.com>
2023-05-29 01:48:42 +0800

    🐎 ci: 增加图片识别OCR模块

--- youtube.py
+++ youtube.py
@@ -9,6 +9,11 @@ from urllib import request, parse
 import os
 import sys
 import subprocess
+# 图像识别
+import easyocr
+# windows下需要先下载模型文件  https://blog.csdn.net/Loliykon/article/details/114334699
+reader = easyocr.Reader(['ch_sim','en'])
+
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
 logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT)
@@ -29,29 +34,59 @@ logging.basicConfig(level=logging.INFO)
 # 隔一段时间获取二维码
 # ffmpeg -i "$(yt-dlp -g qmRkvKo-KbQ | head -n 1)" -vframes 1 dist/last.jpg
 
+youtube_log = []
+
 logging.info(f'===========================================================================开始获取节点信息...')
-for index in range(60):
+for index in range(100):
     subprocess.call(f'ffmpeg -y -i "$(yt-dlp -g qmRkvKo-KbQ | head -n 1)" -vframes 1 dist/last.jpg',shell=True)
+    sleep(2)
     try:
+        youtube_log.append(f'=====================================节点:{index+1}信息======================================================')
         # 处理生成的二维码 生成节点信息
         data:str = qr_recognize(f'dist/last.jpg')
+        logging.info(f'===============================================================================raw_data: {data}')
+        ocr_result = reader.readtext('dist/last.jpg')
+        # 服务器
+        youtube_log.append(f'==============================================================================={ocr_result[1][1]}') # type: ignore
+        log.info(f'==============================================================================={ocr_result[1][1]}') # type: ignore
+        # 地区
+        youtube_log.append(f'==============================================================================={ocr_result[2][1]}')     # type: ignore
+        log.info(f'==============================================================================={ocr_result[2][1]}')     # type: ignore
+        # 类型
+        youtube_log.append(f'==============================================================================={ocr_result[3][1]}:{ocr_result[4][1]}')     # type: ignore
+        log.info(f'==============================================================================={ocr_result[3][1]}:{ocr_result[4][1]}')     # type: ignore
+        # 速度
+        youtube_log.append(f'==============================================================================={ocr_result[5][1]}')     # type: ignore
+        log.info(f'==============================================================================={ocr_result[5][1]}')     # type: ignore
+        # 测速时间
+        youtube_log.append(f'==============================================================================={ocr_result[6][1]}:{ocr_result[7][1].replace(".",":")}')     # type: ignore
+        log.info(f'==============================================================================={ocr_result[6][1]}:{ocr_result[7][1].replace(".",":")}')     # type: ignore
+        # 二维码更新时间
+        youtube_log.append(f'==============================================================================={ocr_result[10][1]}:{ocr_result[11][1].replace(".",":")}')     # type: ignore
+        log.info(f'==============================================================================={ocr_result[10][1]}:{ocr_result[11][1].replace(".",":")}')     # type: ignore
+        # 节点数量
+        youtube_log.append(f'==============================================================================={ocr_result[12][1]}')     # type: ignore
+        log.info(f'==============================================================================={ocr_result[12][1]}')     # type: ignore
     except:
-        subprocess.call(f'ffmpeg -i "$(yt-dlp -g qmRkvKo-KbQ | head -n 1)" -vframes 1 dist/last.jpg',shell=True)
-        data:str = qr_recognize(f'dist/last.jpg')
-    logging.info(f'===============================================================================已获取到节点: {data}')
+        continue
     sub_res = requests.get(f'https://sub.xeton.dev/sub?target=quanx&url={parse.quote(data)}&insert=false')
     sub_res_list: list[str] = sub_res.text.split('\n')
     for index,subitem in enumerate(sub_res_list):
         try:
-            if subitem == '[server_local]' and sub_res_list[index+1].startswith(('shadowsocks ','vmess','trojan','socks5','http')):
+            if subitem == '[server_local]' and sub_res_list[index+1] not in ['','[filter_local]']:
+                # 有效qx订阅节点
                 # 添加到目标节点中
                 youtube_nodes.append(sub_res_list[index+1])
                 # 去重
                 youtube_nodes = list(set(youtube_nodes))
-                logging.info(f'==============================================================================节点池有: {len(youtube_nodes)}个节点')
+                logging.info(f'==============================================================================当前节点池有: {len(youtube_nodes)}个节点')
         except:
             continue
-    sleep(30)
+    # 如果youtube_nodes和ocr_result[12][1]节点数量大于或相同则爬取结束
+    if len(youtube_nodes)>=(int(ocr_result[12][1].split(':')[1])): # type: ignore
+        break
+    sleep(20)
 
+open('dist/youtube.log','w+').write('\n'.join(youtube_log))
 open('dist/youtube.list','w+').write('\n'.join(youtube_nodes))
 logging.info(f'=========================================================================节点更新完成!')
