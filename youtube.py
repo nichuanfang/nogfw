9a5d6001f966ed6cc78c2ed8ec3f7bc4650fee73
倪传方 <1290274972@qq.com>
2023-05-28 17:15:01 +0800

    🐞 fix: use ActionChains to solve videos

--- youtube.py
+++ youtube.py
@@ -3,6 +3,7 @@
 import requests
 from myselenium.my_selenium import get_driver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from myselenium.my_selenium import BeautifulSoup
 import time
 import logging
@@ -16,9 +17,12 @@ driver = get_driver('firefox')
 
 driver.get('https://www.youtube.com/watch?v=qmRkvKo-KbQ')
 driver.maximize_window()
-time.sleep(5)
+time.sleep(10)
 
 open('page_source','w+',encoding="UTF-8").write(driver.page_source)
+
 # 模拟点击播放youtube
-driver.find_element(By.CSS_SELECTOR, ".ytp-play-button").click()
+element = driver.find_element(By.CSS_SELECTOR, ".ytp-play-button")
+ActionChains(driver).move_to_element(element).click().perform()
+
 driver.save_screenshot('dist/res.png')
