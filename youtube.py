cb9dc94e519f5c4708c6b7f702c6e45d414a85e9
倪传方 <1290274972@qq.com>
2023-05-29 02:35:44 +0800

    🐎 ci: cache ocr models

--- youtube.py
+++ youtube.py
@@ -11,6 +11,24 @@ import sys
 import subprocess
 # 图像识别
 import easyocr
+
+data = 'trojan://29645da0-a43a-4ec4-95a1-57eb5e92be44@kr1.microsoft-orgwly.vip:443?allowInsecure=1#%E9%9F%A9%E5%9B%BD-1.04MB%2Fs%28Youtube%3A%E4%B8%8D%E8%89%AF%E6%9E%97%29'
+sub_res = requests.get(f'https://sub.xeton.dev/sub?target=quanx&url={parse.quote(data)}&insert=false')
+sub_res_list: list[str] = sub_res.text.split('\n')
+for index,subitem in enumerate(sub_res_list):
+        if subitem == '[server_local]' and sub_res_list[index+1] not in ['','[filter_local]']:
+            # 有效qx订阅节点
+            pass
+
+
+
+
+
+
+
+
+
+
 # windows下需要先下载模型文件  https://blog.csdn.net/Loliykon/article/details/114334699
 reader = easyocr.Reader(['ch_sim','en'],model_storage_directory='ocr_models')
 
@@ -46,30 +64,9 @@ for index in range(100):
         data:str = qr_recognize(f'dist/last.jpg')
         logging.info(f'===============================================================================raw_data: {data}')
         ocr_result = reader.readtext('dist/last.jpg')
-        # 服务器
-        youtube_log.append(f'==============================================================================={ocr_result[1][1]}') # type: ignore
-        logging.info(f'==============================================================================={ocr_result[1][1]}') # type: ignore
-        # 地区
-        youtube_log.append(f'==============================================================================={ocr_result[2][1]}')     # type: ignore
-        logging.info(f'==============================================================================={ocr_result[2][1]}')     # type: ignore
-        # 类型
-        youtube_log.append(f'==============================================================================={ocr_result[3][1]}:{ocr_result[4][1]}')     # type: ignore
-        logging.info(f'==============================================================================={ocr_result[3][1]}:{ocr_result[4][1]}')     # type: ignore
-        # 速度
-        youtube_log.append(f'==============================================================================={ocr_result[5][1]}')     # type: ignore
-        logging.info(f'==============================================================================={ocr_result[5][1]}')     # type: ignore
-        # 测速时间
-        youtube_log.append(f'==============================================================================={ocr_result[6][1]}:{ocr_result[7][1].replace(".",":")}')     # type: ignore
-        logging.info(f'==============================================================================={ocr_result[6][1]}:{ocr_result[7][1].replace(".",":")}')     # type: ignore
-        # 二维码更新时间
-        youtube_log.append(f'==============================================================================={ocr_result[10][1]}:{ocr_result[11][1].replace(".",":")}')     # type: ignore
-        logging.info(f'==============================================================================={ocr_result[10][1]}:{ocr_result[11][1].replace(".",":")}')     # type: ignore
-        # 节点数量
-        youtube_log.append(f'==============================================================================={ocr_result[12][1]}')     # type: ignore
-        logging.info(f'==============================================================================={ocr_result[12][1]}')     # type: ignore
+        logging.info(f'===============================================================================OCR: {ocr_result}')     # type: ignore
     except Exception as err:
         logging.error(f'==============================={err}==============================================')
-        continue
     sub_res = requests.get(f'https://sub.xeton.dev/sub?target=quanx&url={parse.quote(data)}&insert=false')
     sub_res_list: list[str] = sub_res.text.split('\n')
     for index,subitem in enumerate(sub_res_list):
@@ -84,8 +81,11 @@ for index in range(100):
         except:
             continue
     # 如果youtube_nodes和ocr_result[12][1]节点数量大于或相同则爬取结束
-    if len(youtube_nodes)>=(int(ocr_result[12][1].split(':')[1])): # type: ignore
-        break
+    try:
+        if len(youtube_nodes)>=(int(ocr_result[12][1].split(':')[1])): # type: ignore
+            break
+    except:
+        pass
     sleep(20)
 
 open('dist/youtube.log','w+').write('\n'.join(youtube_log))
