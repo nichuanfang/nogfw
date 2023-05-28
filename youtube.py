6b65226f8a602ff53ad59c54e2b1ceabc918065a
倪传方 <1290274972@qq.com>
2023-05-28 18:17:30 +0800

    EC.element_to_be_clickable

--- youtube.py
+++ youtube.py
@@ -3,6 +3,8 @@
 import requests
 from myselenium.my_selenium import get_driver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from myselenium.my_selenium import BeautifulSoup
 import time
@@ -25,9 +27,12 @@ open('dist/page_source.txt','w+',encoding="UTF-8").write(driver.page_source)
 driver.save_screenshot('dist/pre_res.png')
 
 # 模拟点击播放youtube
-element = driver.find_element(By.XPATH, r'//*[@id="movie_player"]/div[29]/div[2]/div[1]/button')
-element.click()
+# element = driver.find_element(By.CLASS_NAME, 'ytp-play-button')
+# element.click()
 # ActionChains(driver).move_to_element(element).click().perform()
+
+WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable((By.CLASS_NAME, 'ytp-play-button'))).click()
+
 
 time.sleep(5)
 
