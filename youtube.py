16456a6b5eabbae2e31b6365fbcb072a35bf5a9b
倪传方 <1290274972@qq.com>
2023-05-29 16:18:36 +0800

    feature: dump raw data

--- youtube.py
+++ youtube.py
@@ -155,6 +155,9 @@ if __name__ == '__main__':
     # raw数据去重
     # raw_list = ['vmess://eyJ2IjoiMiIsInBzIjoi576O5Zu9LTUuNjNNQi9zKFlvdXR1YmU65LiN6Imv5p6XKSIsImFkZCI6IjIzLjIyNC4xMTAuMTg0IiwicG9ydCI6IjQ0MyIsInR5cGUiOiJub25lIiwiaWQiOiI0MTgwNDhhZi1hMjkzLTRiOTktOWIwYy05OGNhMzU4MGRkMjQiLCJhaWQiOiI2NCIsIm5ldCI6IndzIiwicGF0aCI6Ii9wYXRoLzA4MDcxMjM0MjMxMCIsImhvc3QiOiIiLCJ0bHMiOiJ0bHMifQ==','vmess://eyJ2IjoiMiIsInBzIjoi576O5Zu9LTQuMzlNQi9zKFlvdXR1YmU65LiN6Imv5p6XKSIsImFkZCI6IjE5OC4yLjE5Ni40OSIsInBvcnQiOiI1NDQzNCIsInR5cGUiOiJub25lIiwiaWQiOiI0MTgwNDhhZi1hMjkzLTRiOTktOWIwYy05OGNhMzU4MGRkMjQiLCJhaWQiOiI2NCIsIm5ldCI6InRjcCIsInBhdGgiOiIvIiwiaG9zdCI6IiIsInRscyI6IiJ9']
     raw_list = copy.deepcopy(list(set(raw_list)) )
+
+    open('dist/raw_data.txt', 'w+',encoding='utf-8').write('\n'.join(raw_list))
+    
     clash_dict = generate_clash_config(raw_list,{})
     with open('dist/clash.yml', 'w+',encoding='utf-8') as file:
         file.write(yaml.dump(clash_dict, allow_unicode=True,default_flow_style=False,sort_keys=False))
