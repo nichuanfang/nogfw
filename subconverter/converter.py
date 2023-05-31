f0ad554b8a48464693adcca2207d6a19f4f4d114
nichuanfang <f18326186224@gmail.com>
2023-05-31 18:59:28 +0800

    ✨ feat: generate_template_ini

--- subconverter/converter.py
+++ subconverter/converter.py
@@ -9,8 +9,8 @@ clash_pattern = r'{clash}'
 mixed_pattern = r'{mixed}'
 
 
-with open('subconverter/generate.ini','r+',encoding='utf-8') as generate_file:
-    generate_ini = generate_file.read()
+with open('subconverter/generate_template.ini','r+',encoding='utf-8') as generate_template:
+    generate_template_ini = generate_template.read()
 
 def add_quanx(nodes:list[str]):
     """添加qx节点
@@ -19,8 +19,8 @@ def add_quanx(nodes:list[str]):
         nodes (list[str]): 节点
     """   
     url = '|'.join(nodes)
-    new_generate_ini = re.sub(qx_pattern,f'{url}',generate_ini)
-    open('subconverter/generate.ini','w+',encoding='utf-8').write(new_generate_ini)
+    generate_ini = re.sub(qx_pattern,f'{url}',generate_template_ini)
+    open('subconverter/generate.ini','w+',encoding='utf-8').write(generate_ini)
 
 def add_clash(nodes:list[str]):
     """添加clash节点
@@ -29,8 +29,8 @@ def add_clash(nodes:list[str]):
         nodes (list[str]): 节点
     """    
     url = '|'.join(nodes)
-    new_generate_ini = re.sub(clash_pattern,f'{url}',generate_ini)
-    open('subconverter/generate.ini','w+',encoding='utf-8').write(new_generate_ini)
+    generate_ini = re.sub(clash_pattern,f'{url}',generate_template_ini)
+    open('subconverter/generate.ini','w+',encoding='utf-8').write(generate_ini)
 
 def add_mixed(nodes:list[str]):
     """添加mixed节点 小火箭可用 base64加密
@@ -39,5 +39,5 @@ def add_mixed(nodes:list[str]):
         nodes (list[str]): 节点
     """    
     url = '|'.join(nodes)
-    new_generate_ini = re.sub(mixed_pattern,f'{url}',generate_ini)
-    open('subconverter/generate.ini','w+',encoding='utf-8').write(new_generate_ini)
+    generate_ini = re.sub(mixed_pattern,f'{url}',generate_template_ini)
+    open('subconverter/generate.ini','w+',encoding='utf-8').write(generate_ini)
