12f1427eb134e4e4ed06eeec3cad8c47ad54aa71
nichuanfang <37105637+nichuanfang@users.noreply.github.com>
2023-06-01 02:18:00 +0800

    Update handle_qx.py

--- subconverter/handle_qx.py
+++ subconverter/handle_qx.py
@@ -10,17 +10,17 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 with open(f'dist/quanx-sub.txt','r+',encoding='utf-8') as quanx_f:
     quanxs = quanx_f.readlines()
 
-def format():
-    final_quanx = []
-    for quanx in quanxs:
-        if quanx =='[server_local]\n':
-            continue
-        elif quanx == '\n':
-            continue
-        if quanx.startswith(('vmess','trojan','shadowsocks','http','socks')):
-             final_quanx.append(quanx)
-        else:
-            break
 
-    with open(f'dist/quanx-sub.txt','w+',encoding='utf-8') as quanx_wf:
-                quanx_wf.writelines(final_quanx)
+final_quanx = []
+for quanx in quanxs:
+    if quanx =='[server_local]\n':
+        continue
+    elif quanx == '\n':
+        continue
+    if quanx.startswith(('vmess','trojan','shadowsocks','http','socks')):
+         final_quanx.append(quanx)
+    else:
+        break
+
+with open(f'dist/quanx-sub.txt','w+',encoding='utf-8') as quanx_wf:
+            quanx_wf.writelines(final_quanx)
