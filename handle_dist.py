c83dcdbdae60d71ae1f63532a6ee2f27976c2abe
nichuanfang <f18326186224@gmail.com>
2023-06-03 17:25:52 +0800

    🧪 test: 测试clash配置文件生成格式化是否正确

--- handle_dist.py
+++ handle_dist.py
@@ -1,6 +1,5 @@
 from subconverter import handle_clash
 from subconverter import handle_qx
 
-if '__name__' == '__main__':
-    handle_qx.format()
-    handle_clash.format()
+handle_qx.format()
+handle_clash.format()
