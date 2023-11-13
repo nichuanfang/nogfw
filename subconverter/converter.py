49c2c72271ec6a5d84197a27dbc7f69073c58ca2
nichuanfang <f18326186224@gmail.com>
2023-11-13 11:52:00 +0800

    ✨ feat: 测试

--- subconverter/converter.py
+++ subconverter/converter.py
@@ -268,9 +268,10 @@ def handle_nodes(nodes:list[str]):
     for index,sorted_tag_node_key in enumerate(sorted_tag_node_keys):
         sorted_tag_node = tag_node_dict[sorted_tag_node_key]
         if sorted_tag_node_key.__contains__('马克吐温'):
-            print(f'mac2win节点:{sorted_tag_node_key}')
+            print(f'mac2win处理前的节点:{sorted_tag_node_key}')
             # 处理节点 去除特殊标识(例如: youtube不良林) 添加标签 [序号]
             new_nodes.append(tag(sorted_tag_node,f'[{tag_index}] '+sorted_tag_node_key.replace('(频道:马克吐温)','')))
+            print(f'mac2win处理后的节点: {tag(sorted_tag_node,f"[{tag_index}] "+sorted_tag_node_key.replace("(频道:马克吐温)",""))}')
         elif sorted_tag_node_key.__contains__('不良林'):
             # 过滤速度低于1.5Mbps的节点(即速度低于300KB/S的节点)
             speed = proxy_speed(sorted_tag_node_key)
